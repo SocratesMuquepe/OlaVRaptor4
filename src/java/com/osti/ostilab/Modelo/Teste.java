86781a4e8f35c890175ef90300adbfd58ae7ba45
Socrates Muquepe <socycrates@gmail.com>
2016-09-13 15:51:18 +0100

    its my netbeans commit 13

---
+++ src/java/com/osti/ostilab/Modelo/Teste.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.osti.ostilab.Modelo;
+
+/**
+ *
+ * @author len
+ */
+public class Teste {
+    String titulo;
+    int feito_por;
+}
